b176656361e72545139f19feac7fd08bb50099b2
candidosavino <151361995+Cand0s@users.noreply.github.com>
2023-12-16 15:44:17 +0100

    minimal details

--- src/main/java/exceptions/NullVariableException.java
+++ src/main/java/exceptions/NullVariableException.java
@@ -1,12 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Exception.java to edit this template
- */
 package exceptions;
 
 /**
  *
- * @author candi
+ * @author Group_10
  */
 public class NullVariableException extends RuntimeException{
 
